7b0b61a2af552b612579da666e06694e2a4c1436
Parker J. Rule <parker.rule@tufts.edu>
2020-07-08 22:34:34 -0400

    Add simulation module

---
+++ src/queue.js
@@ -0,0 +1,85 @@
+/**
+ * @file Monte Carlo simulation of an M/D/c queue with variable arrival rates.
+ * @author Parker J. Rule (@pjrule)
+ */
+
+function poisson(lambda) {
+    /* Knuth's Poisson sampling algorithm (slow for large λ). */
+    let L = Math.exp(-lambda);
+    let k = 0;
+    let p = 1;
+    while (p > L) {
+        p *= Math.random();
+        k++;
+    }
+    return k - 1;
+}
+
+export const histPercentiles = function(hist, p) {
+    /**
+     * Retrieves the values at (approximately) the specified percentiles
+     * in a histogram.
+     */
+    p.sort((a, b) => a - b);
+    let results = Array(p.length).fill(0);
+    let histKeys = Object.keys(hist).sort((a, b) => a - b);
+    let histIdx = 1;
+    let binLoRank = 0;
+    let binHiRank = hist[histKeys] - 1;
+    let histSize = Object.values(hist).reduce((a, b) => a + b);
+    for (let idx = 0; idx < p.length; idx++) {
+        let nearestRank = Math.max(0, Math.ceil(histSize * p[idx] / 100) - 1);
+        while (binLoRank < nearestRank && histIdx < hist.length) {
+            binLoRank = binHiRank + 1;
+            binHiRank = binLoRank + hist[histKeys[histIdx]] - 1;
+            histIdx++;
+        }
+        results[idx] = hist[histIdx];
+    }
+    return results;
+}
+
+
+export const simulateMdc = function(lambdas, numStations, stepsPerTest, runs) {
+    /**
+     * Simulates an M/D/c queue with variable arrival rates.
+     *
+     * @param TODO
+     * @return TODO
+     */
+    let steps = lambdas.length;
+    let waitTimeHists = Array(steps).fill(null).map((el, i) => new Map());
+    for (let run = 0; run < runs; run++) {
+        let arrivals = lambdas.map(poisson);
+        let arrivalTs = 0;
+        let stations = Array(numStations).fill(-1);
+        for (let serviceTs = 0; serviceTs < steps && arrivalTs < steps; serviceTs++) {
+            for (let s = 0; s < numStations; s++) {
+                // Fill as many stations as possible at each timestep.
+                if (arrivals[arrivalTs] === 0) {
+                    // Once all people who have arrived at a particular timestep
+                    // have been serviced, move to the people who arrived in the
+                    // next timestep.
+                    arrivalTs++;
+                }
+                if (arrivalTs > serviceTs) {
+                    // Can't fulfill demand before it shows up.
+                    break;
+                }
+                if (stations[s] <= serviceTs) {
+                    // The current station is no longer being used.
+                    // Add a new person.
+                    stations[s] = serviceTs + stepsPerTest;
+                    let waitTime = serviceTs - arrivalTs;
+                    if (waitTime in waitTimeHists[serviceTs]) {
+                        waitTimeHists[serviceTs][waitTime] += 1;
+                    } else {
+                        waitTimeHists[serviceTs][waitTime] = 1;
+                    }
+                    arrivals[arrivalTs]--;
+                }
+            }
+        }
+    }
+    return waitTimeHists;
+}
